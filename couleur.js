c4ec0dc0b678dde08131bb910b0f1377310a2eba
Hugocharlet <hugocharlet@gmail.com>
2024-08-06 21:35:53 +0200

    Update couleur.js

--- couleur.js
+++ couleur.js
@@ -1,13 +1,27 @@
 
-        function getRandomColor() {
-            // Générer une couleur hexadécimale aléatoire
-            const letters = '0123456789ABCDEF';
-            let color = '#';
-            for (let i = 0; i < 6; i++) {
-                color += letters[Math.floor(Math.random() * 16)];
-            }
-            return color;
-        }
+		function getRandomPastelColor() {
+			// Liste des couleurs pastel en format RGB
+			const pastelColors = [
+				'rgb(231,170,214)',
+				'rgb(170,231,177)',
+				'rgb(170,176,231)',
+				'rgb(170,231,223)',
+				'rgb(231,218,170)',
+				'rgb(192,231,170)',
+				'rgb(218,170,231)',
+				'rgb(153,175,229)',
+				'rgb(84,135,229)',
+				'rgb(128,229,179)',
+				'rgb(204,195,140)',
+			];
+
+			// Sélectionner un index aléatoire dans le tableau des couleurs
+			const randomIndex = Math.floor(Math.random() * pastelColors.length);
+
+			// Retourner la couleur aléatoire
+			return pastelColors[randomIndex];
+		}
+
 
         function applyRandomColor() {
             // Appliquer la couleur aléatoire à l'élément avec la classe 'color-box'
@@ -20,4 +34,3 @@
 
         // Mettre à jour la couleur toutes les 30 secondes
         setInterval(applyRandomColor, 10000);
-  
